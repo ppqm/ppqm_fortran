3a83cdce21111b91f8f6c20a50d1f6ec06a730f1
Jimmy Charnley Kromann <jimmy@charnley.dk>
2016-08-11 09:48:50 +0200

    update

--- src/main.f90
+++ src/main.f90
@@ -11,8 +11,61 @@ program main
 
     logical error
 
-    ! What SQM methods are we using today?
     character(20) :: method
+
+
+
+    ! Arguments
+    if(command_argument_count() == 0) then
+        call print_help()
+        stop
+    endif
+
+    do i = 1, command_argument_count()
+
+        if(skip) then
+            skip=.False.
+            cycle
+        endif
+
+        call get_command_argument(i, arg)
+
+        select case (arg)
+
+        case ('-d', '--debug')
+            debug = .True.
+
+        case ('-v', '--version')
+            print '(a)', ''
+            print '(2a)', 'version ', 0
+            stop
+
+        case ('-h', '--help')
+            call "heeeelp"
+            stop
+
+        case default
+            inquire(file=arg, exist=exists)
+            if(.not.exists) then
+                print '(a)', ''
+                print '(2a)', 'Unrecognized option or file: ', arg
+                print '(a)', ''
+                stop
+            else
+                filename = arg
+            endif
+
+        end select
+    enddo
+
+    if(filename=='')then
+        call print_error('No input file')
+    endif
+
+
+    !
+    ! Input file
+    !
 
     filename = "input.ini"
     call ini_set_filename(filename)
