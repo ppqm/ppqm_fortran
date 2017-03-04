b3fba6377b78fb682a65946d6967cb996d1f6e50
Jimmy Charnley Kromann <jimmy@charnley.dk>
2017-03-04 16:21:18 +0100

    changed test to fail

--- tests/constants.py
+++ tests/constants.py
@@ -1,19 +1,12 @@
 
 import ppqm as pp
 
-# print pp.constants.atom_index.__doc__
-# print pp.constants.atom_index("he") == 2
-# print pp.constants.atom_index("C") == 6
-# print pp.constants.atom_index("Lu") == 71
-
-
-
 if __name__ == "__main__":
 
-    try:
-        assert pp.constants.atom_index("C") == 5
+    # pp.constants.atom_index.__doc__
 
-    except AssertionError:
-
-        print "Failed"
+    assert pp.constants.atom_index("he") == 2
+    assert pp.constants.atom_index("Lu") == 71
+    assert pp.constants.atom_index("C") == 6
+    assert pp.constants.atom_index("C") == 400
 
